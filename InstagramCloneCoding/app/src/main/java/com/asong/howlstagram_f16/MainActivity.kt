10d7a58eafb5ba574cb62fd0b3ab04799e7e147a
asong57 <asong57@naver.com>
2021-03-11 10:57:53 +0900

    navigation tab & photo upload page

--- InstagramCloneCoding/app/src/main/java/com/asong/howlstagram_f16/MainActivity.kt
+++ InstagramCloneCoding/app/src/main/java/com/asong/howlstagram_f16/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.asong.howlstagram_f16
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.asong.howlstagram_f16.navigation.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.jar.Manifest
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener{
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            R.id.action_home -> {
+                Log.e("check", "home")
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                Log.e("check", "search")
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, gridFragment)
+                    .commit()
+                return true
+            }
+            R.id.action_add_photo -> {
+                if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
+                    startActivity(Intent(this,AddPhotoActivity::class.java))
+                }
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                Log.e("check", "alarm")
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, alarmFragment)
+                    .commit()
+                return true
+            }
+            R.id.action_account -> {
+                Log.e("check", "account")
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_content, userFragment)
+                    .commit()
+                return true
+            }
+
+        }
+      return false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
     }
 }
